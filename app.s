00c73cd7d9e2baf89725c742d733e351a7b52d28
agustin_castillo <agustin.castillo200@mi.unc.edu.ar>
2024-05-28 18:30:09 -0300

    puse el fondo

--- app.s
+++ app.s
@@ -1,112 +1,67 @@
-	//.include "help.s"
-	.include "drawings.s"
+.include "drawings.s"
 
-	.equ SCREEN_WIDTH,   640
-	.equ SCREEN_HEIGH,   480
-	.equ BITS_PER_PIXEL, 32
+.equ SCREEN_WIDTH,   640
+.equ SCREEN_HEIGHT,  480
+.equ BITS_PER_PIXEL, 32
 
-	.equ GPIO_BASE,    0x3f200000
-	.equ GPIO_GPFSEL0, 0x00
-	.equ GPIO_GPLEV0,  0x34
+.equ GPIO_BASE,    0x3f200000
+.equ GPIO_GPFSEL0, 0x00
+.equ GPIO_GPLEV0,  0x34
 
-	.globl main
+.globl main
 
 main:
-	// x0 contiene la direccion base del framebuffer
-	mov x20, x0 // Guarda la dirección base del framebuffer en x20
+    // x0 contiene la direccion base del framebuffer
+    mov x20, x0 // Guarda la dirección base del framebuffer en x20
 
-	//---------------- CODE HERE ------------------------------------
-	// Color de fondo: azul oscuro 
-	#0F061B
-	movz x10, 0x0f06, lsl 16
-	movk x10, 0x1b, lsl 00
+    //---------------- CODE HERE ------------------------------------
+    // Color de fondo: azul oscuro 
+    #0F061B
 
-	mov x2, SCREEN_HEIGH         // Y Size
+    bl degrade
+
+    movz x10, 0x0000, lsl 16
+    movk x10, 0x0000, lsl 00
+
+    mov x2, SCREEN_HEIGHT         // Y Size
 loop1:
-	mov x1, SCREEN_WIDTH         // X Size
+    mov x1, SCREEN_WIDTH         // X Size
 loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4    // Siguiente pixel
-	sub x1,x1,1    // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1    // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
-	// Trato de dibujar una circunsferencia 
-	mov x3, 320 		// Coor x del centro de la circun
-	mov x4, 240		// Coord y del centro de la circuns 
-	mov x5, 150		// Radio de la circuns 
-	
-	// RECORDATORIOS: Tratar de ver porque dibujar_estrella no se pinta del color que quiero
-	// Parametros: x3 = x, x4 = y, w10 = color
-	// Tamano  estatico (por ahora)
-	mov x3, 300
-	mov x4, 300
-	mov w10, 0xFFFFFF
-	bl dibujar_estrella
+    stur w10,[x0]  // Colorear el pixel N
+    add x0,x0,4    // Siguiente pixel
+    sub x1,x1,1    // Decrementar contador X
+    cbnz x1,loop0  // Si no terminó la fila, salto
+    sub x2,x2,1    // Decrementar contador Y
+    cbnz x2,loop1  // Si no es la última fila, salto
 
-	bl draw_satelite
 
-	mov x3, 320
-	mov x4, 240
-	mov x5, 50
-	bl draw_earth
-	//Dibuja saturno
-	movz x10,0xb6,lsl 16
-	movk x10,0x8355,lsl 00
-	bl dibujar_circulo
-	mov x3,320
-	mov x4,240
-	bl bucket
-	add x5,x3,150
-	sub x3,x3,150
-	movz x10,0xa7,lsl 16
-	movk x10,0x7c53,lsl 00
-	bl bridge
-	add x3,x3,3
-	sub x5,x5,3
-	sub x4,x4,30
-	bl bridge
-	sub SP,SP,8
-	stur x3,[SP]
-	mov x3,320
-	add x4,x4,30
-	bl bucket
-	ldur x3,[SP]
-	add SP,SP,8
-	add x4,x4,30
-	bl bridge
-	add x3,x3,10
-	sub x5,x5,10
-	sub x4,x4,90
-	bl bridge
-	add x4,x4,120
-	bl bridge
-	//Termina saturno""
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+    // Dibujar una circunferencia 
+    mov x3, 320         // Coord x del centro de la circunferencia
+    mov x4, 240         // Coord y del centro de la circunferencia
+    mov x5, 150    // Radio de la circuncunferencia
+    mov w10, 0xFFFFFF        // COLOR
+    bl dibujar_circulo
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+    bl fondo_estrella
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
-	// al inmediato se lo refiere como "máscara" en este caso:
-	// - Al hacer AND revela el estado del bit 2
-	// - Al hacer OR "setea" el bit 2 en 1
-	// - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
-	and w11, w10, 0b00000010
-
-	// si w11 es 0 entonces el GPIO 1 estaba liberado
-	// de lo contrario será distinto de 0, (en este caso particular 2)
-	// significando que el GPIO 1 fue presionado
-
-	//---------------------------------------------------------------
-	// Infinite Loop
 
 InfLoop:
-	b InfLoop
+    b InfLoop
+
+
+degrade:
+
+    mov w11, 0x000F06 
+    mov x2, 1         // Y Size
+loop2:
+    mov x1, SCREEN_WIDTH         // X Size
+loop3:
+    stur w11,[x0]  // Colorear el pixel N
+    add x0,x0,4    // Siguiente pixel
+    sub x1,x1,1    // Decrementar contador X
+    cbnz x1,loop3  // Si no terminó la fila, salto
+    sub x2,x2,1    // Decrementar contador Y
+    cbnz x2,loop2  // Si no es la última fila, salto
+ret
+
--- drawings.s
+++ drawings.s
@@ -2,331 +2,195 @@
 .equ SCREEN_WIDTH, 		640
 .equ SCREEN_HEIGH, 		480	
 
-// Estrella 
-// Parametros: x3 = x, x4 = y, w10 = color
-// Tamano  estatico (por ahora)
+// Estrella de tipo 1 (cuadradito amarillo con cuadraditos en la punta)
+// Parametros: x2 = x, x1 = y
+// Tamano y color estaticos (por ahora)
+dibujar_estrella1: 
+    sub SP, SP, 8   // Reservamos espacio en la pila 						
+	stur X30, [SP, 0]   // Guardamos el valor de x30 en la pila
 
-dibujar_estrella: 
-	sub sp, sp, 8
-	stur x30, [sp, 0]
-	mov x1, 10
-	mov x9, x1
-	bl dibujar_triangulo3
+    // Dibujo el cuadrado principal:
+    mov x3, x2  // Posicion x del centro de la estrella
+    mov x4, x1  // Poscion y del centro de la estrella 
+    mov x1, 3  // Ancho del centro de la estrella = 10
+    mov x2, 3  // Altura del centro de la estrella = 10
+    //mov w10, 0xFFCF13   // Color amarillo para la estrella
+    mov w10, 0xFFFFFF
+    bl dibujar_rectangulo
+
+pintar_fila:
+    mov x3, 1
+    mov x4, 4
+    bl pintar_pixel
+ret
+
+fondo_estrella:
+    mov w10, 0xFFFFFF // COLOR
+//----------------filas_del_principio-------------------------
+	mov x3, 600
+	mov x5, 0 //reseteo el radio
+	mov x4, 8 //reseteo la fila
+        mov x11, 75	
+        bl fila_circulos
+
+//mismo circulo, diferente radio
+    mov w10, 0xFFFF00
+	mov x3, 580 // elijo la posicion en el eje x
+	mov x5, 0 //reseteo el radio
+	mov x4, 45 //reseteo la fila
+	mov x11, 58
+    bl fila_circulos
+
+    mov w10, 0xFFFFFF
+    mov x3, 580 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 45 //reseteo la fila
+	mov x11, 58
+        bl fila_circulos
+//hasta aca
+////------------------filas_del_medio-------------------------//
 	
-	add x3, x3, 10
-	add x4, x4, 5
+    mov x3, 639
+	mov x5, 0 //reseteo el radio
+	mov x4, 70 //reseteo la fila
+    mov x11, 71	
+        bl fila_circulos
 
-	mov x1, x9
-	bl dibujar_triangulo3_inv
-
-ldr x30, [sp, 0]
-add sp, sp, 8
-ret
-
-// ====== DIBUJAR PLANETA TIERRA ======
-// Parametros: x3 -> x | x4 ->y | x5 -> radio 
-// Aclaraciones: (x3, x4) son las coordenadas del centro del circulo
-
-draw_earth: 
-	sub sp, sp, 8
-	stur x30, [sp, 0]
-
-	// Circulo grande general 
-	movz w10, 0x7DAA       // Carga los 16 bits menos significativos
-	movk w10, 0x22, lsl 16
-	bl pintar_circulo
-
-ldr x30, [sp, 0]
-add sp, sp, 8
-ret
-// ====== FIN PLANETA TIERRA ======
-
-
-// ====== DIBUJAR SATELITE ======
-// Sin paraemtro por ahora, dibujado en el mismo lugar
-
-draw_satelite:
-	sub sp, sp, 8
-	stur x30, [sp, 0]
-	mov w10, 0xFFFFFF	//EL color del pixel que voy a pintar
-
-	// Circulo para redondear parte izquierda
-	mov x3, 75
-	mov x4, 60 
-	mov x5, 10
-	bl pintar_circulo	// Ver si lo puedo pintar con el bucket del isma 
-
-	// Circulo para redondear parte derecha
-	mov x3, 105
-	mov x4, 60 
-	mov x5, 10
-	bl pintar_circulo	// Ver si lo puedo pintar con el bucket del isma 	
-
-	// Pata de los paneles: 
-
-	// Pata 1: Arriba a la izquierda
-	mov x3, 80 		// Coordenada x donde lo voy a pintar
-	mov x4,	41		// Coordenada y donde lo voy a pintar
-	mov x1, 2
-	mov x2, 10
-	bl dibujar_rectangulo
-
-	// Pata 2: Arriba a la derecha
-	mov x3, 90 		// Coordenada x donde lo voy a pintar
-	mov x4,	41		// Coordenada y donde lo voy a pintar
-	mov x1, 2
-	mov x2, 10
-	bl dibujar_rectangulo
-
-	// Pata 3: Abajo a la izquierda
-	mov x3, 80 		// Coordenada x donde lo voy a pintar
-	mov x4,	71		// Coordenada y donde lo voy a pintar
-	mov x1, 2
-	mov x2, 10
-	bl dibujar_rectangulo
-
-	// Pata 4: Abajo a la derecha
-	mov x3, 90 		// Coordenada x donde lo voy a pintar
-	mov x4,	71		// Coordenada y donde lo voy a pintar
-	mov x1, 2
-	mov x2, 10
-	bl dibujar_rectangulo
-
-	// Paneles:
-
-	// Panel de arriba
-	mov x3, 75 		// Coordenada x donde lo voy a pintar
-	mov x4,	31		// Coordenada y donde lo voy a pintar
-	mov x1, 22
-	mov x2, 15
-	bl dibujar_rectangulo
-
-	// Panel de abajo
-	mov x3, 75 		// Coordenada x donde lo voy a pintar
-	mov x4,	75		// Coordenada y donde lo voy a pintar
-	mov x1, 22
-	mov x2, 15
-	bl dibujar_rectangulo
-
-	// Circulito de la punta 
-	mov x3, 115
-	mov x4, 60 
-	mov x5, 4
-	bl pintar_circulo	// Ver si lo puedo pintar con el bucket del isma 	
-
-
-	// Rectangulos de la cola:
-
-	// Primer recntagulito (el largito)
-	mov x3, 63 		// Coordenada x donde lo voy a pintar
-	mov x4,	55		// Coordenada y donde lo voy a pintar
-	mov x1, 2		// Ancho
-	mov x2, 9		// Altura 
-	bl dibujar_rectangulo
-
-	// Segundo recntagulito (el cortito)
-	mov x3, 61 		// Coordenada x donde lo voy a pintar
-	mov x4,	57		// Coordenada y donde lo voy a pintar
-	mov x1, 3		// Ancho
-	mov x2, 5		// Altura 
-	bl dibujar_rectangulo
-
-	// Antena: 
-
-	// Circulo grande general
-	mov x3, 41
-	mov x4, 59 
-	mov x5, 20
-	bl pintar_circulo	// Ver si lo puedo pintar con el bucket del isma 	
-
-	// Recatngulo para tapar la parte del circulo y que quede un semicirculo
-	// Tener en cuenta si se cambia el color del fondo 
-	movz w10, 0x0f06, lsl 16
-	movk w10, 0x1b, lsl 00
-	mov x3, 17 		// Coordenada x donde lo voy a pintar
-	mov x4,	27		// Coordenada y donde lo voy a pintar
-	mov x1, 35		// Ancho
-	mov x2, 55		// Altura 
-	bl dibujar_rectangulo
-
-	// Rectangulo para terminar el semicirculo
+// mismo circulo, diferente radio
+    mov w10, 0xFFFF00
+	mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 0 //reseteo el radio
+	mov x4, 110 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
 	mov w10, 0xFFFFFF
-	mov x3, 52 		// Coordenada x donde lo voy a pintar
-	mov x4,	42		// Coordenada y donde lo voy a pintar
-	mov x1, 1		// Ancho
-	mov x2, 35		// Altura 
-	bl dibujar_rectangulo
+    mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 110 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
+// hasta aca
+//
+	mov x3, 630
+	mov x5, 0 //reseteo el radio
+	mov x4, 150 //reseteo la fila
+    mov x11, 45	
+        bl fila_circulos
 
-	// Rectangulo para finalizar la cola de la antena
-	mov x3, 42 		// Coordenada x donde lo voy a pintar
-	mov x4,	58		// Coordenada y donde lo voy a pintar
-	mov x1, 10		// Ancho
-	mov x2, 3		// Altura 
-	bl dibujar_rectangulo
+//mismo circulo, diferente radio
+    mov w10, 0xFFFF00
+	mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 0 //reseteo el radio
+	mov x4, 200 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
 
-	// Circulito pra terminar la cola
-	mov x3, 41
-	mov x4, 59 
-	mov x5, 3
-	bl pintar_circulo	// Ver si lo puedo pintar con el bucket del isma 
+    mov w10, 0xFFFFFF
+    mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 200 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
+//hasta aca
+//aca
+	mov x3, 630
+	mov x5, 0 //reseteo el radio
+	mov x4, 250 //reseteo la fila
+    mov x11, 105	
+        bl fila_circulos
 
-	// Parametros del rectangulo obase del satelite 
-	mov x3, 70 		// Coordenada x donde lo voy a pintar
-	mov x4,	51		// Coordenada y donde lo voy a pintar
-	mov x1, 40		// Ancho
-	mov x2, 20		// Altura
-	bl dibujar_rectangulo
+//mismo circulo, diferente radio
+    mov w10, 0xFFFF00
+	mov x3, 580 // elijo la posicion en el eje x
+	mov x5, 0 //reseteo el radio
+	mov x4, 300 //reseteo la fila
+	mov x11, 58
+        bl fila_circulos
 
-ldr x30, [sp, 0]
-add sp, sp, 8
-ret
-// ====== FIN SATELITE ======
+    mov w10, 0xFFFFFF
+    mov x3, 580 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 300 //reseteo la fila
+	mov x11, 58
+        bl fila_circulos
+//
+	mov x3, 640
+	mov x5, 0 //reseteo el radio
+	mov x4, 360 //reseteo la fila
+    mov x11, 80	
+        bl fila_circulos
 
+//---------relleno-----------//
+	mov x3, 45
+	mov x5, 0 //reseteo el radio
+	mov x4, 245 //reseteo la fila
+    mov x11, 45	
+        bl fila_circulos
 
-draw_saturn:
-	sub SP,SP,8
-	stur x30,[SP]
-	add x11,x3,0
-	lsr x6,x5,2
-	add x7,x4,0
-	add x8,x4,0
-	movz x10,0xb6,lsl 16
-	movk x10,0x8355,lsl 00
-	bl dibujar_circulo
-	bl bucket
-	movz x10,0xa7,lsl 16
-	movk x10,0x7c53,lsl 00
-	bl set_x3_x5
-	bl bridge
-	bl upper_bridge_and_bucket
-	movz x10,0xc8,lsl 16
-	movk x10,0x4903,lsl 00
-	bl upper_bridge_and_bucket
-	movz x10,0xec,lsl 16
-	movk x10,0x8735,lsl 00
-	bl upper_bridge_and_bucket
-	movz x10,0xff,lsl 16 
-	movk x10,0xd8ba,lsl 00
-	bl lower_bridge_and_bucket
-	movz x10,0x33,lsl 16
-	movk x10,0x0000,lsl 00 
-	bl lower_bridge_and_bucket
-	movz x10,0xe0,lsl 16
-	movk x10,0xa06a,lsl 00
-	bl lower_bridge_and_bucket
-	b end_saturn
-upper_bridge_and_bucket:
-	sub SP,SP,8
-	stur x30,[SP]
-	sub x7,x7,x6
-	add x4,x7,0
-	add x3,x11,0
-	bl set_x3_x5
-	bl bridge
-	add x3,x11,0
-	add x4,x7,x6
-	bl bucket
-	ldur x30,[SP]
-	add SP,SP,8
-	ret
-lower_bridge_and_bucket:
-	sub SP,SP,8
-	stur x30,[SP]
-	add x8,x8,x6
-	add x4,x8,0
-	add x3,x11,0
-	bl set_x3_x5
-	bl bridge
-	add x3,x11,0
-	add x4,x8,0
-	bl bucket
-	ldur x30,[SP]
-	add SP,SP,8
-	ret
-end_saturn:
-	ldur x30,[SP]
-	add SP,SP,8
-	ret
+    mov w10, 0xFFFFFF
+    mov x3, 45 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 245 //reseteo la fila
+	mov x11, 45
+        bl fila_circulos
+//
+	mov x3, 20
+	mov x5, 0 //reseteo el radio
+	mov x4, 367 //reseteo la fila
+    mov x11, 20	
+        bl fila_circulos
 
-set_x3_x5:
-	sub SP,SP,8
-	stur x30,[SP]
-	sub SP,SP,8
-	stur x9,[SP]
-	sub SP,SP,8
-	stur x8,[SP]
-	sub SP,SP,8
-	stur x2,[SP]
-	sub SP,SP,8
-	stur x1,[SP]
-	bl dir_pixel
-	add x5,x3,0
-	add x1,x0,0
-	ldur w9,[x0]
-compare_x3:
-	sub x0,x0,4
-	ldur w8,[x0]
-	cmp w8,w9
-	b.eq decr_x3
-	b.ne compare_x5
-decr_x3:
-	sub x3,x3,1
-	b compare_x3
-compare_x5:
-	add x1,x1,4
-	ldur w8,[x1]
-	cmp w8,w9
-	b.eq acrem_x5
-	b.ne end_aux
-acrem_x5:
-	add x5,x5,1
-	b compare_x5
-end_aux:
-	ldur x1,[SP]
-	add SP,SP,8
-	ldur x2,[SP]
-	add SP,SP,8
-	ldur x8,[SP]
-	add SP,SP,8
-	ldur x9,[SP]
-	add SP,SP,8
-	ldur x30,[SP]
-	add SP,SP,8
-	ret
+    mov w10, 0xFFFFFF
+    mov x3, 20 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 367 //reseteo la fila
+	mov x11, 20
+        bl fila_circulos
+//--------------------filas_fianles-----------------------------------//
 
+    mov x3, 639
+	mov x5, 0 //reseteo el radio
+	mov x4, 450 //reseteo la fila
+        mov x11, 71	
+        bl fila_circulos
 
+// mismo circulo, diferente radio
+    mov w10, 0xFFFF00
+	mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 0 //reseteo el radio
+	mov x4, 400 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
 
+    mov w10, 0xFFFFF0
+    mov x3, 600 // elijo la posicion en el eje x
+	mov x5, 1 //reseteo el radio
+	mov x4, 400 //reseteo la fila
+	mov x11, 75
+        bl fila_circulos
+// hasta aca
 
-
-
-
-
-
-
-
-
-
-
-/* 
-dibujar_estrella_fugaz: 
-    mov x3, 200          // Initial x coordinate
-	mov x4, 200          // Initial y coordinate
-	mov x1, 120          // Length of the row
-	mov w10, 0xFFFF   // Color (assuming the color is passed in w10)
-	bl pintar_fila      // Call the function
+//---------------------------------------------------//
 ret
 
-*/
-// ESTRELLA TITILANDO (PARA LA ANIMACION NO SIRVE PARA ESTO)
-// desde x = x3, y = x4, de una longitud de x1 y un color w10
-/* dibujar_estrella2:
-	mov x10, x1			// Me guardo en x10 la longitud para ir decrementando
-	mov x11, x3			// Me guardo en x11 la coordenada inicial de x (es lo que voy a ir modificando)
-loop_est2:
-	bl pintar_pixel		// pintamos un pixel en la coordenada (x, y). No es necesario mover variables ya que usamos los mismo registros pra obtener las coordenadas
-	add x11, x11, 1		// Voy al siguiente pixel en x 
-	sub x10, x10, 1		// Decremento mi contador 
-	cmp x10, 0 		// if mi contador != 0 sigo pintando 
-	b.ne loop_est2		// if mi contado == 0, dejo de pintar ya que pinte toda la longitud de la fila
-ret  */
+fila_circulos:
+    add x6,x3,0
+    add x7,x4,0
+    add x8,x5,0
+    sub SP,SP,8     //reservamos espacio en la pila
+    stur x30,[SP]	// guardamos el valor de x3 en la pila
+
+    bl dibujar_circulo
+    add x3,x6,0
+    add x4,x7,0
+    add x5,x8,0
+    sub x3, x3, x11    // Decrementa en 100 en lugar de 200
+    cbnz x3, fila_circulos // Comprueba si x6 aún es positivo
+    //reseteo los flags
+    mov x3, 0
+    mov x5, 0
+    mov x4, 0
+    ldur x30,[SP]   
+    add SP,SP,8
+ret
 
