92f1bc6702f37a7dc28f8cbbf1562cba6d822566
MilenaPellegrino <milenapellegrinoch@gmail.com>
2024-06-06 00:33:42 -0300

    Commitee mal antes

--- Ejercicio 2/app.s
+++ Ejercicio 2/app.s
@@ -7,60 +7,12 @@
 .equ GPIO_GPFSEL0, 0x00
 .equ GPIO_GPLEV0,  0x34
 
-.equ vel_meteorito, 5	// Velocidad del meteorito 
 .globl main
 
 // Bucle infinito para la animacion 
-	InfLoop:
-		cmp x2, 320	// Nos vamos a mover hasta donde este saturno mas o menos
-		b.ge InfLoop_op_normales
 
-			// Reiniciamos los valores de <x> y de <y>
-			mov x1,0
-			mov x2,0
-			//eor x8,x8,1
-			//mov x5,1
-		InfLoop_op_normales:
-
-		sub x7,x7,1		// Decrementamos para controlar el ritmo de la animacion 
-
-		// Dibujamos todo lo que hay en nuestra pantalla
-		bl fondo_degrade
-		bl fondo_estrella
-
-		bl dibujar_astronauta
-		mov x3, 320
-		mov x4, 240
-		mov x5, 150
-		bl draw_saturn
-
-		mov x3, x1
-		mov x4, x2
-		bl draw_meteorito
-
-		add x1, x1, 1
-		add x2, x2, 1
-		cbnz x7, InfLoop
-
-		sub x1,x1,1
-		sub x2,x2,1
-		ldr x7,=MOV_SOL
-
-		mov x5,0
-
-		mov x0,0xffffff
-		loop:
-			sub x0,x0,1
-			cbnz x0,loop
-		b InfLoop
-
-
+// Funcion normal 
 main:
-<<<<<<< HEAD
-	mov x2, 0	// En x2 guardamos la direccion vertical (y) que vamos a mover al meteorito 
-	mov x1, 0 	// En x1 guardamos la direccion horizontal (x) que vamos a mover al meteorito
-	mov x7, vel_meteorito	// Guardamos en x7 la velocidad del meteorito 
-=======
     // x0 contiene la direccion base del framebuffer
     mov x20, x0 // Guarda la dirección base del framebuffer en x20
 
@@ -92,5 +44,3 @@ moon:
 	bl draw_satelite
 
 	b InfLoop
-
->>>>>>> 220ce6381e58a0c77f6478edd765da026e40d1fe
